324b2c28e341816b0e23f0e1217ec783ab61aa3e
hanna kebede <se.hanna.kebede@gmail.com>
2024-09-12 20:40:55 +0300

    Fix image path and optimize build

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Logo from "@/public/images/logo_1.png"
+import Logo from "@/images/logo_1.png"
 import React from "react";
 import Link from "next/link";
 

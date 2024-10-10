00b712265777f6a3c54ef7fcf434c8414c637a43
hanna kebede <se.hanna.kebede@gmail.com>
2024-10-10 21:18:09 +0300

    adjusted hero

--- components/home/hero.js
+++ components/home/hero.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Section1() {
   return (
   <div
-    className='md:mx-16 mx-5 bg-cover bg-center w-80 h-auto md:rounded-2xl rounded-sm'
+    className='md:mx-16 mx-5 bg-cover bg-center md:w-auto w-80 h-auto md:rounded-2xl rounded-sm'
       style={{
         backgroundImage: `url("/images/bg.png")`, 
       }}>
@@ -12,7 +12,7 @@ function Section1() {
        <h1 className="md:text-8xl text-5xl text-white font-extralight">Welcome to </h1>
        <h1 className="md:text-8xl text-5xl text-yellow-400 font-semibold">Adey Gifts</h1>
        <h1 className='h-1 md:w-12 w-6 bg-white  md:mt-6 mt-2 md:mb-10 mb-4'></h1>
-       <h3 className="text-gray-300 pr-">Discover a delightful array of unique and handcrafted <br/> gifts perfect for any occasion. A boutique <br/> collection of quirky and charming gifts that add a touch of magic <br/> to every celebration.</h3>
+       <h3 className="text-gray-300 ">Discover a delightful array of unique and handcrafted <br/> gifts perfect for any occasion. A boutique <br/> collection of quirky and charming gifts that add a touch of magic <br/> to every celebration.</h3>
     </div>
     </div>
   )
--- models/User.js
+++ models/User.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+// const userSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-const User = mongoose.models.User || mongoose.model('User', userSchema);
-export default User;
+// const User = mongoose.models.User || mongoose.model('User', userSchema);
+// export default User;

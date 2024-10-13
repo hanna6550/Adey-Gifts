ed41e4181e44214cae4a9c8128e78f8be5debd82
hanna kebede <se.hanna.kebede@gmail.com>
2024-10-13 16:53:46 +0300

    bug fixed

--- components/footer.js
+++ components/footer.js
@@ -14,10 +14,10 @@ function Footer() {
      <Image alt='adey logo ' src={Logo2} className='md:w-32 w-28 md:h-40 h-32 mx-auto'/>
      <h1 className="text-xl font-thin text-yellow-400 text-center py-5"> Explore a diverse collection of <br/> handpicked gifts that inspire joy and <br/> celebration.</h1>
      <div className="flex justify-center gap-5 text-white">
-      <FaFacebookF size={20} />
-      <FaTelegram size={20} />
-      <FaInstagram size={20}/>
-      <FaTiktok size={20} />
+     <a href="https://www.tiktok.com"> <FaFacebookF size={20} /> </a>
+     <a href="https://t.me/adeygifts"> <FaTelegram size={20} /> </a>
+     <a href="https://www.instagram.com/adey.gifts/"> <FaInstagram size={20}/> </a>
+     <a href="https://www.tiktok.com/@amanuelz7"> <FaTiktok size={20} /> </a>
      </div>
      <div className="flex justify-center space-x-3 text-white pt-4">
      <LuPhoneCall size={30} className="pt-1"/> <h1 className="text-xl"> 0919208746 </h1>

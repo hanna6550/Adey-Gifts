05069c0da1ce712a96047ec94eb2d73ecb069219
hanna kebede <se.hanna.kebede@gmail.com>
2024-12-05 16:01:06 +0300

    pushing

--- components/home/hero.js
+++ components/home/hero.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Section1() {
   return (
   <div
-    className='md:mx-16 mx-5 bg-cover bg-center md:w-auto w-80 md:h-96 h-72 md:rounded-2xl rounded-sm'
+    className='md:mx-16 mx-5 bg-cover bg-center md:w-auto w-96 md:h-96 h-72 md:rounded-2xl rounded-sm'
       style={{
         backgroundImage: `url("/images/bg.png")`, 
       }}>
@@ -12,7 +12,7 @@ function Section1() {
        <h1 className="md:text-7xl text-5xl text-white font-extralight md:pt-0 pt-7">Welcome to </h1>
        <h1 className="md:text-7xl text-5xl text-yellow-400 font-semibold mb-2">Adey Gifts</h1>
        <h1 className='h-1 md:w-14 w-8 bg-white  md:mt-6 mt-2 md:mb-10 mb-4'></h1>
-       <h3 className="text-gray-300 font-light md:text-xl text-base md:mt-10 mt-4">Discover a delightful array of unique and handcrafted <br/> gifts perfect for any occasion. A boutique <br/> collection of quirky and charming gifts that add a touch of magic <br/> to every celebration.</h3>
+       <h3 className="text-gray-300 font-light md:text-xl text-base md:mt-10 mt-4">Discover a delightful array of unique and handcrafted <br/> gifts perfect for any occasions. A boutique <br/> collection of quirky and charming gifts that add a touch of magic <br/> to every celebration.</h3>
     </div>
     </div>
   )
